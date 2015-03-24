85848775d4fd24c795b3567ab0b524258328faef
Aoshi Sugihara <aoshi@gb-aoshi-2.local>
2015-03-24 11:44:43 +0900

    修正

--- hello.js
+++ hello.js
@@ -3,6 +3,7 @@ module.exports = function (req, res, next) {
   var botPayload = {
     text : 'Hello, ' + userName + '!'
   };
-
-  return res.status(200).json(botPayload);
-}
+  if (userName !== "hellobot") {
+    return res.status(200).json(botPayload);
+  };
+};
